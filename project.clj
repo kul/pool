3381a28887e34390c11e10240f17f8b1335ad8ad
Kuldeep Bora <kuldeep.bora@inmobi.com>
2022-11-15 10:21:00 +0530

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pool "0.3.0"
+(defproject pool "0.3.1-SNAPSHOT"
   :description "Pooling Library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
